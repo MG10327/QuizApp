eb342b9d89b2f93f04e7ba1240095d452585aabd
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-19 06:42:59 -0400

    added basic question setup

--- src/components/Quiz/quiz.jsx
+++ src/components/Quiz/quiz.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Quiz.scss'
+import {data} from '../../../assets/data'
 
 export const quiz = () => {
+
+    let [index, setIndex] = useState(2);
+    let [question, setQuestion] = useState(data[index])
+
   return (
     <div className='container'>
         <h1>Quiz App</h1>
         <hr/>
-        <h2>Which device is required for the Internet Connection</h2>
+        <h2>{index+1}. {question.question}</h2>
         <ul>
-            <li>Moden</li>
-            <li>Router</li>
-            <li>LAN Cable</li>
-            <li>Pen Drive</li>
+            <li>{question.option1}</li>
+            <li>{question.option2}</li>
+            <li>{question.option3}</li>
+            <li>{question.option4}</li>
         </ul>
         <button>Next</button>
         <div className="index">1 of 5 questions</div>
